ab6061078de6c04e212cec615b58776875eb96ba
Animesh Kambhale <animeshkambhale2001@gmail.com>
2024-06-06 18:18:41 +0530

    push

--- src/Components/header/HeaderSocials.jsx
+++ src/Components/header/HeaderSocials.jsx
@@ -4,9 +4,9 @@ import {BsDribbble, BsGithub, BsLinkedin} from 'react-icons/bs'
 export default function HeaderSocials() {
   return (
     <div className="header__socials">
-        <a href='https://linkedin.com' target='_blank'><BsLinkedin/></a>
-        <a href='https://github.com' target='_blank'><BsGithub/></a>
-        <a href='https://dribble.com' target='_blank'><BsDribbble/> </a>
+        <a href='https://www.linkedin.com/in/animesh-kambhale-7b7818238/' target='_blank'><BsLinkedin/></a>
+        <a href='https://github.com/animeshak' target='_blank'><BsGithub/></a>
+        {/* <a href='https://dribble.com' target='_blank'><BsDribbble/> </a> */}
     </div>
     )
 }
--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './portfolio.css'
 import Weather from '../../assets/weather-app.png' 
+import Dashboard from '../../assets/dashboard.png'
 
 export default function Portfolio() {
   return (
@@ -12,7 +13,7 @@ export default function Portfolio() {
           <div className="portfolio__item-image">
             <img src={Weather} alt="img1" />
           </div>
-            <h3>Weather App Using React</h3>
+            <h3>Weather Application</h3>
             <div className="portfolio__item-cta">
             <a href="https://github.com/animeshak/weather-app" className='btn' target='_blank'>Github</a>
             <a href="https://weather-app-sigma-eight-94.vercel.app/" className='btn btn-primary'>Live Demo</a>
@@ -20,15 +21,15 @@ export default function Portfolio() {
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src="https://cdn.dribbble.com/userupload/9631208/file/original-3660996893c469420f37a2022ad57d5e.jpg?resize=1504x1128" alt="img1" />
+            <img src={Dashboard} alt="img1" />
           </div>
-            <h3>This is portfolio item title</h3>
+            <h3>Data Visualization Dashboard</h3>
             <div className="portfolio__item-cta">
             <a href="https://github.com/animesh-ak" className='btn' target='_blank'>Github</a>
             <a href="https://github.com/animesh-ak" className='btn btn-primary'>Live Demo</a>
             </div>
         </article>
-        <article className="portfolio__item">
+        {/* <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src="https://cdn.dribbble.com/userupload/9631208/file/original-3660996893c469420f37a2022ad57d5e.jpg?resize=1504x1128" alt="img1" />
           </div>
@@ -47,7 +48,7 @@ export default function Portfolio() {
             <a href="https://github.com/animesh-ak" className='btn' target='_blank'>Github</a>
             <a href="https://github.com/animesh-ak" className='btn btn-primary'>Live Demo</a>
             </div>
-        </article>
+        </article> */}
       </div>
     </section>
   )
